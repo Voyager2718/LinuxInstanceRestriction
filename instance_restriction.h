b5bb79fb265ab9b6fcfb0ecbb88a7e8378d30159
Voyager2718 <Voyager2718@users.noreply.github.com>
2017-09-20 15:37:19 +0800

    Create instance_restriction.h

---
+++ instance_restriction.h
@@ -0,0 +1,24 @@
+#ifndef INSTANCE_RESTRICTION
+#define INSTANCE_RESTRICTION
+
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define bool int
+#define true 1
+#define false 0
+
+#define MAX_INSTANCE 2
+#define MAX_INSTANCE_DIGITS 10
+#define LOCK_PREFIX "/tmp/instance"
+#define LOCK_SUFFIX ".lock"
+
+bool ReachedMaximumInstance();
+
+#endif
